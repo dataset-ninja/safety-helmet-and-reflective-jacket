52dc01d65bbd4e0d775d5a58d08a363383fab38f
erastov-alex <erastov.alex@gmail.com>
2023-10-23 18:30:30 +0300

    upd convert, settings, gitignore

--- src/settings.py
+++ src/settings.py
@@ -13,8 +13,8 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "Safety Helmet and Reflective Jacket"
+PROJECT_NAME_FULL: str = "Safety Helmet and Reflective Jacket"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
